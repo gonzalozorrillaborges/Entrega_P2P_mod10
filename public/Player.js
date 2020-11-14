425f0cfe4a91799a05d1c33c619017ab637d143c
gonzalozorrillaborges <74463589+gonzalozorrillaborges@users.noreply.github.com>
2020-11-14 16:04:42 -0400

    Add files via upload

---
+++ public/Player.js
@@ -0,0 +1,77 @@
+/**
+ * Personaje principal del juego. Hereda de la clase Character.
+ * @extends Character
+ */
+class Player extends Character {
+    /**
+     * Inicializa un jugador
+     * @param game {Game} La instancia del juego al que pertenece el jugador
+     */
+    constructor (game) {
+        const height = PLAYER_HEIGHT * game.width / 100,
+            width = PLAYER_WIDTH * game.width / 100,
+            x = game.width / 2 - width / 2,
+            y = game.height - height,
+            speed = PLAYER_SPEED,
+            myImage = PLAYER_PICTURE,
+            myImageDead = PLAYER_PICTURE_DEAD;
+
+
+
+        super(game, width, height, x, y, speed, myImage, myImageDead);
+
+        //AGREGADOS PARA EL EJERCICIO
+        this.lives = 3;
+    }
+
+    /**
+     * Actualiza los atributos de posición del jugador y los disparos en función de las teclas pulsadas
+     */
+    update () {
+        if (!this.dead) {
+            switch (this.game.keyPressed) {
+            case KEY_LEFT:
+                if (this.x > this.speed) {
+                    this.x -= this.speed;
+                }
+                break;
+            case KEY_RIGHT:
+                if (this.x < this.game.width - this.width - this.speed) {
+                    this.x += this.speed;
+                }
+                break;
+            case KEY_SHOOT:
+                this.game.shoot(this);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Mata al jugador
+     */
+    collide() {
+        //AGREGADOS PARA EL EJERCICIO
+        //if (!this.dead) {
+        //SE AGREGA LA CONDICION DE VIDAS EN CERO
+        if(!this.dead){
+            this.lives--;
+            console.log('Vidas: '+this.lives);
+            document.getElementById('livesli').innerHTML = 'Lives: '+this.lives;
+            super.collide();
+            setTimeout(() => {  
+                this.image.src = this.myImage;
+                this.dead=false;  
+            }, 2000);
+        }
+
+        if(this.dead&&(this.lives === 0)){
+            //console.log('Vidas: '+this.lives);
+            setTimeout(() => {
+                this.game.endGame();
+            }, 2000);
+            super.collide();
+        }
+
+    }
+}
